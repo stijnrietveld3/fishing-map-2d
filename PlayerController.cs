65c94ce7a8194e8d1e9b85be5c1f2f15a43c8dc9
stijnrietveld3 <stijnrietveld3@gmail.com>
2026-03-11 19:04:13 +0100

    Add PlayerController script

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,2 +1,26 @@
-// PlayerController.cs
-// This script manages player movement and actions, including WASD movement.
+using UnityEngine;
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed = 5f;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        if (animator != null) {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+        }
+    }
+
+    void FixedUpdate() {
+        rb.velocity = movement * moveSpeed;
+    }
+}
