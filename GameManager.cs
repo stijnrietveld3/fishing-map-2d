823caaf63fd353f49ed6b8e1cf26b98748189d01
stijnrietveld3 <stijnrietveld3@gmail.com>
2026-03-11 19:04:20 +0100

    Add GameManager.cs for handling game state and player tracking.

--- GameManager.cs
+++ GameManager.cs
@@ -1,2 +1,45 @@
-// GameManager.cs
-// This script manages overall game states, level tracking, and game flow.
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int playerMoney;
+    public int currentLevel;
+    public int fishIndex;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        playerMoney = 0;
+        currentLevel = 1;
+        fishIndex = 0;
+    }
+
+    public void AddMoney(int amount)
+    {
+        playerMoney += amount;
+    }
+
+    public void LevelUp()
+    {
+        currentLevel++;
+    }
+
+    public void SetFishIndex(int index)
+    {
+        fishIndex = index;
+    }
+}
